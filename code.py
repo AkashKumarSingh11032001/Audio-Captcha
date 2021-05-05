40c56f07d4ff896c7401ef2eae3a9c6843948439
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-05-05 22:34:50 +0530

    code

--- code.py
+++ code.py
@@ -7,7 +7,7 @@ from captcha.audio import AudioCaptcha
 captcha_audio = AudioCaptcha()
 
 # Define data
-captcha_data = captcha_audio.generate('123')
+captcha_data = captcha_audio.generate("123")
 
 # Write to the file
-captcha_audio.write('123', 'captcha_sample_audio.wav')
+captcha_audio.write("123", "captcha_sample_audio.wav")
